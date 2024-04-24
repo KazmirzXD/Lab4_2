b2b289cf9c12a2410533a0c9800a0d5726078853
KazmirzXD <161707953+KazmirzXD@users.noreply.github.com>
2024-04-24 13:12:10 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    try {
+      Service s = new Service();
+      Scanner scanner = new Scanner(System.in);
+
+      s.addStudent(new Student("Krzysztof", 20));
+      s.addStudent(new Student("Janusz", 40));
+
+      System.out.println("Wybierz opcje:");
+      System.out.println("1 - Dodaj");
+      System.out.println("2 - Wypisz studentów");
+      System.out.println("3 - Wyjście");
+
+      int choice = scanner.nextInt();
+      switch (choice) {
+        case 1:
+          System.out.print("Imie: ");
+          String name = scanner.next();
+          System.out.print("Wiek: ");
+          int age = scanner.nextInt();
+          s.addStudent(new Student(name, age));
+          break;
+        case 2:
+          var students = s.getStudents();
+          for (Student current : students) {
+            System.out.println(current.ToString());
+          }
+          break;
+        case 3:
+          System.out.println("Wyjście z programu.");
+          System.exit(0);
+          break;
+        default:
+          System.out.println("Zły wybór.");
+      }
+    } catch (IOException e) {
+
+    }
+  }
+}
